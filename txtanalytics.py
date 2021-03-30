95d6e5724611b08bea46451f813fa7acc21da4e0
Tommy Wang <tommy860215@gmail.com>
2021-03-30 20:44:17 +0800

    update commit

--- txtanalytics.py
+++ txtanalytics.py
@@ -9,3 +9,17 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言的平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 5:
+		new.append(d)
+print(len(new))
+print('一共有', len(new), "資料小於5")
+
+good = []
+for d in data:
+	if 'ramen' in d:
+		good.append(d)
+print(good)
+print('ramen' in good)
